28aabc3e8b14db192c0c9ccdb73d060bf76e2c45
Francis-Berrier <francisbernard_son@hotmail.com>
2025-03-21 23:28:09 +0100

    site complet avec responsive design

--- src/pages/Erreur/index.jsx
+++ src/pages/Erreur/index.jsx
@@ -12,7 +12,7 @@ function Erreur() {
     return (
         <section className={styles.erreur}>
             <h1>404</h1>
-            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            <div className={styles.erreurText}><span>Oups! La page que </span><span>vous demandez n'existe pas.</span></div>
             <a onClick={handleClick}>Retourner sur la page d’accueil</a>
         </section>
     )
